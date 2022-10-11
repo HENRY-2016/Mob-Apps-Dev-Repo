fe14c99a5376c7080751367d6e3052c189e67bd4
HENRY-2016 <jacob2016henry@gmail.com>
2022-10-11 20:07:57 +0300

    11-10-2022-Updates

---
+++ kamProperty/admin/App.js
@@ -0,0 +1,164 @@
+import 'react-native-gesture-handler';
+import React, {useState} from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import {View,Image,ActivityIndicator,Text} from "react-native";
+
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+} from '@react-navigation/drawer';
+
+import { Entypo,} from '@expo/vector-icons';
+import { NavigationIcon } from './src/screens/Functions';
+// UIs
+import styles from "./src/screens/stylesheet";
+import Dashboard from './src/screens/Dashboard';
+import OrdersNew from './src/screens/OrdersNew';
+import OrdersCleared  from './src/screens/OrdersCleared';
+import OrdersPending  from './src/screens/OrdersPending';
+import PaymentsNew from './src/screens/PaymentsNew';
+import PaymentsFull from './src/screens/PaymentsFull';
+import PaymentsHalf from './src/screens/PaymentsHalf';
+import PaymentsCleared from './src/screens/PaymentsCleared';
+import PaymentsPending from './src/screens/PaymentsPending';
+
+import Icon1 from "./src/imgs/dashboard/1.png";
+import Icon3 from "./src/imgs/dashboard/3.png";
+import Icon4 from "./src/imgs/dashboard/4.png";
+import Icon5 from "./src/imgs/dashboard/5.png";
+import Icon6 from "./src/imgs/dashboard/6.png";
+import Icon7 from "./src/imgs/dashboard/7.png";
+import Icon8 from "./src/imgs/dashboard/8.png";
+
+
+function CustomDrawerContent(props) {
+  return (
+    <View style={{flex:1}}>
+        <DrawerContentScrollView {...props}>
+          <View style={styles.drawerUserView}>
+            <Entypo name="user" size={100} style={styles.draweUserIcones}/>
+          </View>
+          <View>
+            <Text  style={ styles.drawerUserName}>Admin</Text>
+          </View>
+        <DrawerItemList {...props} />
+      </DrawerContentScrollView>
+    </View>
+  );
+}
+
+
+
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigetion() {
+  return (
+    <Drawer.Navigator initialRouteName="Dashboard"
+      useLegacyImplementation
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+
+      screenOptions={{
+        drawerStyle: {backgroundColor: '#1F75FE',width: 255},
+        drawerActiveBackgroundColor:'#af4035',
+        drawerInactiveTintColor:'#fff',
+        drawerActiveTintColor:'#fff',
+        drawerLabelStyle:{fontSize: 17,marginLeft:-20}
+      }}
+    >
+      
+        <Drawer.Screen name="Dashboard"   component={Dashboard} options={{headerShown: false,swipeEnabled: false,
+        title: 'Dashboard',unmountOnBlur: true,
+        drawerIcon:() => (<Entypo name="home" size={26} color="white" />),
+        }} />
+        <Drawer.Screen name="OrdersNew"  component={OrdersNew} options={{headerShown: false,swipeEnabled: false,
+        title: 'Orders New',unmountOnBlur: true,
+        drawerIcon:() => (<NavigationIcon image={Icon1} />),
+        }}/>
+      
+        <Drawer.Screen name="OrdersCleared" component={OrdersCleared} options={{headerShown: false,swipeEnabled: false,
+        title: 'Orders Cleared',unmountOnBlur: true,
+        drawerIcon:() => (<NavigationIcon image={Icon4} />),
+        }} />
+
+        <Drawer.Screen name="OrdersPending"  component={OrdersPending} options={{headerShown: false,swipeEnabled: false,
+        title: 'Orders Pending',unmountOnBlur: true,
+        drawerIcon:() => (<NavigationIcon image={Icon3} />),
+        }}/>
+        <Drawer.Screen name="PaymentsNew" component={PaymentsNew} options={{headerShown: false,swipeEnabled: false,
+        title: 'Payments New',unmountOnBlur: true,
+        drawerIcon:() => (<NavigationIcon image={Icon5} />),
+        }} />
+        
+        <Drawer.Screen name="PaymentsFull" component={PaymentsFull} options={{headerShown: false,swipeEnabled: false,
+        title: 'Payments Full',unmountOnBlur: true,
+        drawerIcon:() => (<NavigationIcon image={Icon8} />),
+        }} />
+        <Drawer.Screen name="PaymentsHalf" component={PaymentsHalf} options={{headerShown: false,swipeEnabled: false,
+        title: 'Payments  Half',unmountOnBlur: true,
+        drawerIcon:() => (<NavigationIcon image={Icon8} />),
+        }} />
+        <Drawer.Screen name="PaymentsPending" component={PaymentsPending} options={{headerShown: false,swipeEnabled: false,
+        title: 'Payments Pending',unmountOnBlur: true,
+        drawerIcon:() => (<NavigationIcon image={Icon7} />),
+        }} />
+        <Drawer.Screen name="PaymentsCleared" component={PaymentsCleared} options={{headerShown: false,swipeEnabled: false,
+        title: 'Payments Cleared',unmountOnBlur: true,
+        drawerIcon:() => (<NavigationIcon image={Icon6} />),
+        }} />
+        
+    </Drawer.Navigator>
+  );
+}
+
+export default function App() {
+
+  const [showSplashScreen , setshowSplashScreen] = useState(true);; 
+  setTimeout(()=>{ setshowSplashScreen(false)},4000);
+  return (
+  
+    <>
+    <NavigationContainer >
+
+      {
+        showSplashScreen ? 
+        (
+          <>
+            <View style={styles.mainViewSplah} >
+                  <View style={{height:30}} ></View>
+                  <View style={[styles.splashScreenTextView,styles.splashScreenTextView1]}>
+                        <Text style={styles.splashScreenText}>Hello Welcome To </Text>
+                    </View>
+
+                    <View style={styles.splashScreenView} >
+                        <Image
+                        style={styles.splashScreenImage}
+                        source={require('./assets/bg-1.png')}
+                        />
+                    </View>
+
+                      {/* <View style={{height:30}}></View> */}
+                      <View style={[styles.splashScreenTextView,styles.splashScreenTextView2]}>
+                          <Text style={styles.splashScreenText}>Kam Beddings</Text>
+                      </View>
+
+                      <View style={styles.activityIdicaterView}>
+                          <View style={styles.activityIdicatercontainer}>
+                          <ActivityIndicator size="large" style={{marginTop:-50}} color="#fff" />
+
+                          </View>
+                      </View>
+                  
+              </View>
+          </>
+        )
+        :
+        <DrawerNavigetion/>
+      }
+        
+    </NavigationContainer>
+    </>
+  );
+}
+
+// eas build -p android --profile preview
---
+++ kamProperty/admin/src/screens/Colours.js
@@ -0,0 +1,38 @@
+//https://www.w3schools.com/colors/colors_picker.asp
+// https://bestofreactjs.com/repo/intellidev1991-react-native-image-slider-box
+export const COLORS = {
+  
+  black: "#000",
+  ///////////////////////////////////
+  colourNumberZero:"#ff4d4d",
+  colourNumberOne:"#af4035",
+  colourNumberTwo:"#6CB4EE",
+  // colourNumberFour:"#F7F6E7",
+  colourNumberFour:"#a0785a",
+  colourNumberFive:"#E7E6E1",
+  // colourNumberSix:"#e60000",
+  colourNumberSix:"#6495ED",
+
+  
+  tableThColor:"#C70039",
+  // colourNumberEight:"#FF0000",
+  // colourNumberNone:"F7F6E7",
+  
+  
+
+  white: "#fff",
+  subLinkNaviColour:"#C70039",
+
+
+}
+
+
+export const OTHERS = {
+  bigbtnfontsize: 20,
+  orderbtnfontsize: 17,
+  producttextfontsize: 16,
+  black: "#000",
+  mainBgColour:"#F9B208",
+  // /////////////
+  fontsize20:20,
+}
